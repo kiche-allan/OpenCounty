5d8d19b6e29ebebf51af3f7d31d96c72a53b3c5d
Allan Kiche <tle2819@students.mu.ac.ke>
2023-05-31 15:31:13 +0300

    alla

---
+++ src/constants/index.js
@@ -0,0 +1,238 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star, agric, education, health } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "all indicators",
+    title: "All indicators",
+  },
+  {
+    id: "factsheets",
+    title: "Factsheets",
+  },
+  {
+    id: "county comparisons",
+    title: "County comparisons",
+  },
+  {
+    id: "infographics",
+    title: "Infographics",
+  }
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "ANNUAL TARGETED REVENUE",
+    content:
+      "100 million",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "ANNUAL ACTUAL REVENUE",
+    content:
+      "52.2 Million",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Percentage Raised:",
+    content:
+      "87.7%",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      `25 degrees centigrade
+      Temperature9 Annual Average Highest) 
+      
+      9 degrees centigrade
+      Temperature (Annual Average Lowest)
+      
+      17 degrees centigrade
+      Temperature (Annual Everage)
+      
+      535 Sq. Km
+      Water Mass 
+      `,
+    name: "Health",
+    title: "Source - Kenya National Bureau of Statistics",
+    img: health,
+  },
+  {
+    id: "feedback-2",
+    content:
+      `25 degrees centigrade
+      Temperature9 Annual Average Highest) 
+      
+      9 degrees centigrade
+      Temperature (Annual Average Lowest)
+      
+      17 degrees centigrade
+      Temperature (Annual Everage)
+      
+      535 Sq. Km
+      Water Mass 
+      `,
+    name: "Education",
+    title: " Source - Kenya National Bureau of Statistics(2014)",
+    img: education,
+  },
+  {
+    id: "feedback-3",
+    content:
+      `25 degrees centigrade
+      Temperature9 Annual Average Highest) 
+      
+      9 degrees centigrade
+      Temperature (Annual Average Lowest)
+      
+      17 degrees centigrade
+      Temperature (Annual Everage)
+      
+      535 Sq. Km
+      Water Mass `,
+    name: "Agriculture",
+    title: "Source - Kenya National Bureau of Statistics",
+    img: agric,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Population",
+    value: "610,082",
+    paragraph: ``
+  },
+  {
+    id: "stats-2",
+    title: "Area Size",
+    value: "2,086 km²",
+  },
+  {
+    id: "stats-3",
+    title: "Total GDP",
+    value: "$	3,883",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
